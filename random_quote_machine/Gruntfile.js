846161600bc508519352ec1b7e02b77cc1a6305d
harpreet singh <harpeetsingh500@gmail.com>
2017-01-13 16:31:02 -0600

    fixed the font awesome icon(was not showing up) and minified the css

--- random_quote_machine/Gruntfile.js
+++ random_quote_machine/Gruntfile.js
@@ -1,5 +1,14 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    copy: {
+      main: {
+        expand: true,
+        cwd: "bower_components/components-font-awesome/",
+        src: ["css/*.css",
+              "fonts/*"],
+        dest: "public/stylesheets/font-awesome/"
+      }
+    },
     uglify: {
       my_target: {
         files: {
@@ -25,7 +34,8 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks("grunt-contrib-uglify");
   grunt.loadNpmTasks("grunt-bower-concat");
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-copy");
 
   grunt.registerTask("default", ["bower_concat", "concat", "uglify"]);
 };
